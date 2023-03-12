04d41d1a3daa98847f7ef2f9dc196d728700bebd
mshsvnv <mshsvnv@mail.ru>
2023-03-12 20:29:33 +0300

    add examples and 2 versions of lab

---
+++ lab_03/version1/part1.asm
@@ -0,0 +1,54 @@
+PUBLIC letter, num
+EXTERN output_letter: near, output_num: near, make_new: far, output_new: far
+
+SSTK SEGMENT para STACK 'STACK'
+    db 100 dup(0)
+SSTK ENDS
+
+DSEG SEGMENT PARA COMMON 'DATA'
+    letter_msg db 10, 13, "Input letter: $"
+    num_msg db 10, "Input number: $"
+    new_msg db 10, "New letter: $"
+    
+    letter db 0
+    num db 0
+DSEG ENDS
+
+CSEG SEGMENT PARA PUBLIC 'CODE'
+    assume CS:CSEG, DS:DSEG
+main:
+    mov AX, DSEG
+    mov DS, AX
+
+    mov dx, offset letter_msg
+    mov AH, 9
+    int 21h
+
+    mov AH, 7       ; ввод символа без эхо
+    int 21h
+
+    mov DS:letter, al ; занесение буквы в letter
+    call output_letter
+
+    mov dx, offset num_msg
+    mov AH, 9
+    int 21h
+
+    mov AH, 7       ; ввод символа без эхо
+    int 21h
+
+    mov DS:num, al  ; занесение цифры в num
+    call output_num
+
+    call make_new
+
+    mov dx, offset new_msg
+    mov AH, 9
+    int 21h
+    call output_new
+
+    mov AH, 4Ch
+    int 21h
+
+CSEG ENDS
+END main
---
+++ lab_03/version2/part2.asm
@@ -0,0 +1,47 @@
+PUBLIC output_letter, output_num, make_new, output_new
+
+EXTERN letter: byte, num: byte, new_letter: byte
+
+CSEG SEGMENT PARA PUBLIC 'CODE'
+    assume CS:CSEG
+
+output_letter proc near  ; вывод исходной буквы
+    mov dl, letter
+    mov AH, 2
+    int 21h
+
+    ret
+output_letter endp
+
+output_num proc near    ; вывод цифры
+    mov dl, num
+    mov AH, 2
+    int 21h
+
+    ret
+output_num endp
+
+make_new proc far   ; вычитание
+    sub num, 48
+    mov cl, num
+
+    mov al, letter
+    mov new_letter, al
+
+    shift:
+        sub new_letter, 1
+        loop shift
+
+    ret
+make_new endp
+
+output_new proc far ; вывод новой строки
+    mov dl, new_letter
+    mov AH, 2
+    int 21H
+
+    ret
+output_new endp
+
+CSEG ENDS
+END
